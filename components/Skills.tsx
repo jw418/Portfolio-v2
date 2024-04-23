a1f5deadec52713ef457b075c7d4b6919b08fdf7
jw418 <ju.wolff@free.fr>
2024-04-24 00:59:34 +0200

    edit svg args name

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -147,9 +147,9 @@ export default function Skills(props: {
                   y2="208"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
                 <line
                   x1="192"
@@ -158,9 +158,9 @@ export default function Skills(props: {
                   y2="192"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="16"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
                 ></line>
               </svg>
               Shadcn
