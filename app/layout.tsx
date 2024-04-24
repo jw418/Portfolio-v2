3c8d1bac88e9dc6928d1fa05832832dfb2afb50a
jw418 <ju.wolff@free.fr>
2024-04-24 02:30:51 +0200

    update nextjs version

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,13 @@ import { ThemeProvider } from "@/components/theme-provider";
 export const metadata: Metadata = {
   title: "JW418 Portfolio",
   description: "JW418 Portfolio",
-  viewport: "width=device-width, initial-scale=1",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: 1,
 };
 
 export default function RootLayout({
